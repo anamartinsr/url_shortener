2c3dbfc11c9529557f65bd0957436da371b4e13e
anamartinsr <anapribbeiro@gmail.com>
2026-01-21 16:53:03 -0300

    feat(logging): add structured logs to URL controller actions

--- src/infrastructure/http/controllers/url.controller.ts
+++ src/infrastructure/http/controllers/url.controller.ts
@@ -6,10 +6,12 @@ import { GetOriginalUrlUseCase } from "../../../application/usecases/getOriginal
 import { CreateUrlDTO } from "../../../interfaces/dto/create-url.dto";
 import { GetUrlDTO } from "../../../interfaces/dto/get-url.dto";
 
+import { logger } from "../../logger/logger";
+
 export class UrlController {
   constructor(
     private createShortUrlUseCase: CreateShortUrlUseCase,
-    private getOriginalUrlUseCase: GetOriginalUrlUseCase
+    private getOriginalUrlUseCase: GetOriginalUrlUseCase,
   ) {}
 
   private createSchema = Joi.object<CreateUrlDTO>({
@@ -18,6 +20,9 @@ export class UrlController {
 
   create = async (req: Request, res: Response) => {
     const dto: CreateUrlDTO = req.body;
+
+    logger.info({ longUrl: req.body.long_url }, "Request to shorten URL");
+
     const { error } = this.createSchema.validate(dto);
 
     if (error) {
@@ -26,6 +31,9 @@ export class UrlController {
     const entity = await this.createShortUrlUseCase.execute(dto);
     const host = req.get("host");
     const protocol = req.protocol;
+
+    logger.info({ shortcode: entity.shortcode }, "Short URL created");
+
     return res.status(201).json({
       shortcode: entity.shortcode,
       short_url: `${protocol}://${host}/${entity.shortcode}`,
@@ -37,10 +45,14 @@ export class UrlController {
       shortcode: req.params.shortcode,
     };
 
+    logger.info({ shortcode: req.params.shortcode }, "Redirect request");
+
     const longUrl = await this.getOriginalUrlUseCase.execute(dto);
     if (!longUrl) {
+      logger.warn({ shortcode: dto.shortcode }, "Shortcode not found");
       return res.status(404).json({ message: "Not found" });
     }
+
     return res.redirect(302, longUrl);
   };
 
